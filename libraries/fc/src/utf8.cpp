ca1519618f8619d2aa17847ea793b3cf30c9fcc3
Daniel Larimer <dan@bitshares.org>
2016-05-24 19:24:03 -0400

    updating websocket and adding new utf8 code

--- libraries/fc/src/utf8.cpp
+++ libraries/fc/src/utf8.cpp
@@ -3,14 +3,34 @@
 #include "utf8/checked.h"
 #include "utf8/core.h"
 #include "utf8/unchecked.h"
+#include <websocketpp/utf8_validator.hpp>
 
 #include <assert.h>
+#include <fc/log/logger.hpp>
+#include <iostream>
 
 namespace fc {
 
    bool is_utf8( const std::string& str )
    {
+      auto itr = utf8::find_invalid(str.begin(), str.end()); 
       return utf8::is_valid( str.begin(), str.end() );
+   }
+
+   string prune_invalid_utf8( const string& str ) {
+      string result;
+
+      auto itr = utf8::find_invalid(str.begin(), str.end()); 
+      if( itr == str.end() ) return str;
+
+      result = string( str.begin(), itr );
+      while( itr != str.end() ) {
+         ++itr;
+         auto start = itr;
+         itr = utf8::find_invalid( start, str.end()); 
+         result += string( start, itr );
+      }
+      return result;
    }
 
    void decodeUtf8(const std::string& input, std::wstring* storage)
