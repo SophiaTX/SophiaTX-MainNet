6c9dff2227a2f8340c75f3616173c4e157984c69
Daniel Larimer <dan@bitshares.org>
2016-09-14 10:30:48 -0400

    define to/from variant for boost interprocess deque and vector

---
+++ libraries/fc/include/fc/interprocess/container.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <fc/io/raw.hpp>
+#include <fc/variant.hpp>
+
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/containers/string.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+#include <boost/interprocess/containers/map.hpp>
+#include <boost/interprocess/containers/deque.hpp>
+
+
+namespace fc {
+
+   namespace bip = boost::interprocess;
+
+    template<typename... T >
+    void to_variant( const bip::deque< T... >& t, fc::variant& v ) {
+      std::vector<variant> vars(t.size());
+      for( size_t i = 0; i < t.size(); ++i ) {
+         vars[i] = t[i];
+      }
+      v = std::move(vars);
+    }
+
+    template<typename T, typename... A>
+    void from_variant( const fc::variant& v, bip::deque< T, A... >& d ) {
+      const variants& vars = v.get_array();
+      d.clear();
+      d.resize( vars.size() );
+      for( uint32_t i = 0; i < vars.size(); ++i ) {
+         from_variant( vars[i], d[i] );
+      }
+    }
+
+    template<typename... T >
+    void to_variant( const bip::vector< T... >& t, fc::variant& v ) {
+      std::vector<variant> vars(t.size());
+      for( size_t i = 0; i < t.size(); ++i ) {
+         vars[i] = t[i];
+      }
+      v = std::move(vars);
+    }
+
+    template<typename T, typename... A>
+    void from_variant( const fc::variant& v, bip::vector< T, A... >& d ) {
+      const variants& vars = v.get_array();
+      d.clear();
+      d.resize( vars.size() );
+      for( uint32_t i = 0; i < vars.size(); ++i ) {
+         from_variant( vars[i], d[i] );
+      }
+    }
+}
