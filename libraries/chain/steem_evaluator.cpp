3f1481bf13c85da9a6157da894489ef41c676da3
Josef Sevcik <josef.sevcik@gmail.com>
2018-06-05 13:19:44 +0200

    Update steem_evaluator.cpp
    Additional delete checks

--- libraries/chain/steem_evaluator.cpp
+++ libraries/chain/steem_evaluator.cpp
@@ -322,6 +322,12 @@ void account_delete_evaluator::do_apply(const account_delete_operation& o)
       auto account_auth_itr = _db.find< account_authority_object, by_account >( o.account );
 
       verify_authority_accounts_exist( _db, o.owner, o.account, authority::owner );
+       
+      const auto& by_witness_name_idx = _db.get_index< witness_index >().indices().get< by_name >();
+      auto wit_itr = by_witness_name_idx.find( o.account );
+      
+      FC_ASSERT(wit_itr == by_witness_name_idx.end() , "Cannot delete account as long as there is active witness associated to it");
+      FC_ASSERT(account.balance.amount == 0 && account.vesting_shares.amount == 0, "Cannot delete account with remaining balance" );
 
       _db.remove(account);
 
