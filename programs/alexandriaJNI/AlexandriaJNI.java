056b250f5ad1e8e952a5197789b74b8864ea07c4
Matus Kysel <matus.kysel@sophiatx.com>
2018-07-04 15:38:19 +0200

    Added tests for jni library

--- programs/alexandriaJNI/AlexandriaJNI.java
+++ programs/alexandriaJNI/AlexandriaJNI.java
@@ -1,7 +1,6 @@
 public class AlexandriaJNI {
    static {
       System.loadLibrary("alexandriaJNI"); // Load native library at runtime
-                                   // hello.dll (Windows) or libhello.so (Unixes)
    }
  
    private native String[] generateKeyPair();
@@ -17,8 +16,29 @@ public class AlexandriaJNI {
  
    // Test Driver
    public static void main(String[] args) {
+      String tx = "{\"id\":0,\"result\":{\"ref_block_num\":28367,\"ref_block_prefix\":785299212,\"expiration\":\"2018-07-04T11:19:03\",\"operations\":[[\"account_create\",{\"fee\":\"0.100000 SPHTX\",\"creator\":\"GN3Ug8ou6tiE4kWvyopJBJmZgcw\",\"name_seed\":\"sanjiv\",\"owner\":{\"weight_threshold\":1,\"account_auths\":[],\"key_auths\":[[\"SPH6vh1vH3DTzFj2NUpZgpXfNACxUGsXThSpwVLXh9KaYAnJtrUpz\",1]]},\"active\":{\"weight_threshold\":1,\"account_auths\":[],\"key_auths\":[[\"SPH6vh1vH3DTzFj2NUpZgpXfNACxUGsXThSpwVLXh9KaYAnJtrUpz\",1]]},\"memo_key\":\"SPH6vh1vH3DTzFj2NUpZgpXfNACxUGsXThSpwVLXh9KaYAnJtrUpz\",\"json_metadata\":\"{}\"}]],\"extensions\":[],\"signatures\":[]}}";
       AlexandriaJNI test = new AlexandriaJNI();
-      System.out.println("getPublicKey output: " + test.getPublicKey("5JPwY3bwFgfsGtxMeLkLqXzUrQDMAsqSyAZDnMBkg7PDDRhQgaV"));
+      String[] key_pair = test.generateKeyPair();
+      // System.out.println(key_pair[0]);
+      // System.out.println(test.getPublicKey(key_pair[1]));
+      if(!test.getPublicKey(key_pair[1]).equals(key_pair[0]))
+      {
+          System.out.println("getPublicKey failed");
+      }
+      String digest = test.getTransactionDigest(tx, "0000000000000000000000000000000000000000000000000000000000000000");
+      String signature = test.signDigest(digest, key_pair[1]);
+      if(!test.verifySignature(digest, key_pair[0], signature))
+      {
+          System.out.println("Transaction signing failed");
+      }
+
+      String[] key_pairB = test.generateKeyPair();
+      String memo = "testabc";
+      String enctryped = test.encryptMemo(memo, key_pair[1], key_pairB[0]);
+      if(!test.decryptedMemo(enctryped, key_pairB[1], key_pair[0]).equals(memo))
+      {
+         System.out.println("Encryption failed");
+      }
    }
 }
 
