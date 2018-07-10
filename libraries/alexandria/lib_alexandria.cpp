5665bf9ae9c1bc8cc3f31d746dbffe9dd7b4897e
Matus Kysel <matus.kysel@sophiatx.com>
2018-07-10 12:52:40 +0200

    Fixed crash on sing and send

--- libraries/alexandria/lib_alexandria.cpp
+++ libraries/alexandria/lib_alexandria.cpp
@@ -679,13 +679,19 @@ fc::ecc::compact_signature alexandria_api::sign_digest(digest_type digest, strin
 annotated_signed_transaction alexandria_api::send_and_sign_operation(operation op, string pk) {
    try{
       auto tx = create_simple_transaction(op);
-      broadcast_transaction(add_signature(tx, sign_digest(get_transaction_digest(tx), pk)));
+      auto digest = get_transaction_digest(tx);
+      auto signature = sign_digest(digest, pk);
+      tx = add_signature(tx, signature);
+      broadcast_transaction(tx);
    }FC_CAPTURE_AND_RETHROW((op)(pk))
 }
 
 annotated_signed_transaction alexandria_api::send_and_sign_transaction(signed_transaction tx, string pk){
    try{
-      broadcast_transaction(add_signature(tx, sign_digest(get_transaction_digest(tx), pk)));
+      auto digest = get_transaction_digest(tx);
+      auto signature = sign_digest(digest, pk);
+      auto tx_ = add_signature(tx, signature);
+      broadcast_transaction(tx_);
    }FC_CAPTURE_AND_RETHROW((tx)(pk))
 }
 
