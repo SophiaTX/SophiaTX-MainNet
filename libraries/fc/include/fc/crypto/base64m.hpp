6767dc96b5ba8187b1918ee354f50989dcf78272
Matus Kysel <matus.kysel@sophiatx.com>
2018-08-08 14:55:15 +0200

    Changed base64 account names to support -_

--- libraries/fc/include/fc/crypto/base64m.hpp
+++ libraries/fc/include/fc/crypto/base64m.hpp
@@ -6,4 +6,5 @@ std::string base64m_encode(unsigned char const* bytes_to_encode, unsigned int in
 inline std::string base64m_encode(char const* bytes_to_encode, unsigned int in_len) { return base64m_encode( (unsigned char const*)bytes_to_encode, in_len); }
 std::string base64m_encode( const std::string& enc );
 std::string base64m_decode( const std::string& encoded_string);
+std::string normalize_to_base64m(const std::string& base64_string);
 }  // namespace fc
