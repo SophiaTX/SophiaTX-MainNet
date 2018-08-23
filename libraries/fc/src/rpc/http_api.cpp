09c18616788a3567a806eaab4e100431785939ae
Matus Kysel <matus.kysel@sophiatx.com>
2018-08-23 13:23:05 +0200

    Handling json rpc without id

--- libraries/fc/src/rpc/http_api.cpp
+++ libraries/fc/src/rpc/http_api.cpp
@@ -109,6 +109,7 @@ void http_api_connection::on_request( const fc::http::request& req, const fc::ht
          {
             try
             {
+               FC_ASSERT( call.id.valid(), "id member of json rpc is required" );
                auto result = _rpc_state.local_call( call.method, call.params );
                resp_body = fc::json::to_string( fc::rpc::response( *call.id, result ) );
                resp_status = http::reply::OK;
@@ -117,7 +118,7 @@ void http_api_connection::on_request( const fc::http::request& req, const fc::ht
          }
          catch ( const fc::exception& e )
          {
-            resp_body = fc::json::to_string( fc::rpc::response( *call.id, error_object{ 1, e.to_detail_string(), fc::variant(e)} ) );
+            resp_body = fc::json::to_string( fc::rpc::response( (call.id.valid()) ? *call.id : 1, error_object{ 1, e.to_detail_string(), fc::variant(e)} ) );
             resp_status = (send_error) ? http::reply::InternalServerError : http::reply::OK;
          }
       }
