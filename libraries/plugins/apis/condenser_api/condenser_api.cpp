12c93578ad6d87ea10666221f0f55a6782dc16f9
Jakub Fonadel <jakub.fornadel@sophiatx.com>
2018-09-07 14:27:44 +0200

    minor code refactor

--- libraries/plugins/apis/condenser_api/condenser_api.cpp
+++ libraries/plugins/apis/condenser_api/condenser_api.cpp
@@ -795,7 +795,7 @@ void condenser_api::api_startup()
       my->_custom_api = custom->api;
 
    auto subscribe = appbase::app().find_plugin< subscribe::subscribe_api_plugin>();
-   if ( subscribe )
+   if ( subscribe != nullptr)
       my->_subscribe_api = subscribe->api;
 }
 
