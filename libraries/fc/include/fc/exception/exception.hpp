0aef38fc62a7ba33d762ea6c4e49ce97cf19b11a
Jakub Fonadel <jakub.fornadel@sophiatx.com>
2018-10-23 17:43:06 +0200

    cyclic instance bug fix

--- libraries/fc/include/fc/exception/exception.hpp
+++ libraries/fc/include/fc/exception/exception.hpp
@@ -215,15 +215,6 @@ namespace fc
         exception_factory();
         std::unordered_map<int64_t,base_exception_builder*> _registered_exceptions;
    };
-#define FC_REGISTER_EXCEPTION(r, unused, base) \
-   fc::exception_factory::instance().register_exception<base>();
-
-#define FC_REGISTER_EXCEPTIONS( SEQ )\
-   static bool exception_init = []( __attribute__((unused)) bool* )->bool{ \
-    BOOST_PP_SEQ_FOR_EACH( FC_REGISTER_EXCEPTION, v, SEQ )  \
-      return true; \
-   }( &exception_init ); \
-
 
 #define FC_DECLARE_DERIVED_EXCEPTION( TYPE, BASE, CODE, WHAT ) \
    class TYPE : public BASE  \
