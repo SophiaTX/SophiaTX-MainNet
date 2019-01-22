6ac9a887e1c2c209a44ab25163200e9c984f04cc
Jakub Fonadel <jakub.fornadel@sophiatx.com>
2019-01-22 17:50:03 +0100

    supress warning by pragam only for gcc > 8

--- tests/include/unit_test.hpp
+++ tests/include/unit_test.hpp
@@ -1,6 +1,10 @@
+#if __GNUC__ >= 8
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wformat-overflow="
+#endif
 
 #include <boost/test/included/unit_test.hpp>
 
+#if __GNUC__ >= 8
 #pragma GCC diagnostic push
+#endif
