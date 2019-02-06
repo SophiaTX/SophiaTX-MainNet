a6e64c8fc220a4e356301acb3963112d9d4f5578
Jakub Fonadel <jakub.fornadel@sophiatx.com>
2019-02-06 15:48:25 +0100

    refactor smart_contract tests

--- tests/smart_contracts/db_resource_pool_tests.cpp
+++ tests/smart_contracts/db_resource_pool_tests.cpp
@@ -1,6 +1,8 @@
 #include <boost/test/unit_test.hpp>
 #include <fc/exception/exception.hpp>
 #include <sophiatx/smart_contracts/db_resource_pool.hpp>
+#include <sophiatx/utilities/lru_resource_pool.hpp>
+#include <SQLiteCpp/SQLiteCpp.h>
 #include <boost/filesystem/operations.hpp>
 #include <iostream>
 
@@ -14,42 +16,24 @@ BOOST_AUTO_TEST_CASE( db_resource_pool_tests )
    {
       BOOST_TEST_MESSAGE( "Testing: db_resource_pool" );
       boost::filesystem::path data_dir(boost::filesystem::current_path() / "databases/");
-      constexpr uint32_t max_pool_size = 3;
+      if (boost::filesystem::exists(data_dir) == false) {
+         boost::filesystem::create_directory(data_dir);
+      }
 
-      db_resource_pool resource_pool(data_dir, max_pool_size);
-      resource_pool.create_resource("acc1");
-      resource_pool.create_resource("acc2");
-      resource_pool.create_resource("acc3");
-      resource_pool.create_resource("acc4");
-
-
-      BOOST_TEST_MESSAGE( "--- Test max number of resources enabled in pool" );
-      BOOST_CHECK_EQUAL(max_pool_size, resource_pool.size());
+      constexpr uint32_t max_pool_size = 1;
+      sophiatx::utilities::LruResourcePool<std::string, SQLite::Database> resourcePool(max_pool_size);
 
 
-      BOOST_TEST_MESSAGE( "--- Test if the least used resource was deleted" );
-      BOOST_CHECK_THROW( resource_pool.get_resource("acc1", false/*do not create resource*/), sophiatx::smart_contracts::resource_error );
+      SQLite::Database& acc1_db = resourcePool.getResourceAut("acc1", data_dir.generic_string() + "acc1.db", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
+      auto& acc2_db             = resourcePool.getResourceAut("acc2", data_dir.generic_string() + "acc2.db", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
 
 
-      BOOST_TEST_MESSAGE( "--- Test failure when trying to get non-existing resource" );
-      BOOST_CHECK_THROW( resource_pool.get_resource("acc5", false/*do not create resource*/), sophiatx::smart_contracts::resource_error );
-
-
-      BOOST_TEST_MESSAGE( "--- Test creating non-existing resource with get_resource method" );
-      BOOST_CHECK_NO_THROW( resource_pool.get_resource("acc5") );
-
-
-      // There are now resources mapped to accounts acc3, acc4, acc5 that are ordered(according to the last access_time): acc3, acc4, acc5
-      resource_pool.get_resource("acc3", false/*do not create resource*/); // use resource acc3 -> updates last access_time
-      // Now there should be present resources in order: acc4, acc5, acc3
-      resource_pool.create_resource("acc6");                               // create new resource for acc6
-      // Now there should be present resources in order: acc5, acc3, acc6
-      BOOST_TEST_MESSAGE( "--- Test if last_access time of resource was updated when get_resource called" );
-      BOOST_CHECK_NO_THROW( resource_pool.get_resource("acc3") );
-
+      BOOST_TEST_MESSAGE( "--- Test if created database have different files" );
+      BOOST_CHECK_NE( acc1_db.getFilename(), acc2_db.getFilename() );
 
       BOOST_TEST_MESSAGE( "--- Test if the least used resource was deleted" );
-      BOOST_CHECK_THROW( resource_pool.get_resource("acc4", false/*do not create resource*/), sophiatx::smart_contracts::resource_error );
+      BOOST_CHECK_EQUAL( resourcePool.size(), max_pool_size );
+      BOOST_CHECK_EQUAL( resourcePool.getResource("acc1").has_value(), false );
 
       boost::filesystem::remove_all(data_dir);
    }
