2beb1a5593e353df0a09c4e0cd8c6eb4be8edc36
Jakub Fonadel <jakub.fornadel@sophiatx.com>
2019-02-28 09:27:46 +0100

    hack for maybe-uninitialized???

--- libraries/plugins/apis/alexandria_api/alexandria_api_impl.cpp
+++ libraries/plugins/apis/alexandria_api/alexandria_api_impl.cpp
@@ -559,7 +559,11 @@ DEFINE_API_IMPL(alexandria_api_impl, get_account_bandwidth) {
    optional<chain::account_bandwidth_object> bandwidth = _account_bandwidth_api->get_account_bandwidth( { args.account } ).bandwidth;
    if (bandwidth.valid() == true) {
       result.bandwidth = account_bandwidth(*bandwidth);
-      result.bandwidth->next_block_num_reset = bandwidth->last_block_num_reset + SOPHIATX_LIMIT_BANDWIDTH_BLOCKS;
+
+      uint64_t last_block_num_reset = 0;
+      last_block_num_reset = bandwidth->last_block_num_reset;
+
+      result.bandwidth->next_block_num_reset = last_block_num_reset + SOPHIATX_LIMIT_BANDWIDTH_BLOCKS;
    }
 
    return result;
