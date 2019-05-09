8a66b9ef9f21ee3da464e633acf2016bae231b10
jakub.fornadel <jakub.fornadel@gmail.com>
2019-05-09 18:16:04 +0200

    refactor

--- tests/include/unit_test.hpp
+++ tests/include/unit_test.hpp
@@ -1,10 +1,6 @@
-#       define SUPPRESSING
-#       pragma GCC diagnostic push
-#       pragma GCC diagnostic ignored "-Wformat-overflow="
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-overflow="
 
 #include <boost/test/included/unit_test.hpp>
 
-#ifdef SUPPRESSING
-#   undef SUPPRESSING
-#   pragma GCC diagnostic pop
-#endif
+#pragma GCC diagnostic pop
