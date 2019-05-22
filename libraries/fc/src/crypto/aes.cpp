71cf8fe6d8977f8ab4a7be7b3a9402797cb5cfd7
Matus Kysel <matus.kysel@sophiatx.com>
2019-05-22 11:46:03 +0200

    removed boost mutex from aes

--- libraries/fc/src/crypto/aes.cpp
+++ libraries/fc/src/crypto/aes.cpp
@@ -8,8 +8,8 @@
 
 #include <fc/log/logger.hpp>
 
-#include <fc/thread/thread.hpp>
-#include <boost/thread/mutex.hpp>
+#include <mutex>
+
 #include <openssl/opensslconf.h>
 #ifndef OPENSSL_THREADS
 # error "OpenSSL must be configured to support threads"
@@ -374,7 +374,7 @@ std::vector<char> aes_load( const fc::path& file, const fc::sha512& key )
 */
 struct openssl_thread_config
 {
-  static boost::mutex* openssl_mutexes;
+  static std::mutex* openssl_mutexes;
   static unsigned long get_thread_id();
   static void locking_callback(int mode, int type, const char *file, int line);
   openssl_thread_config();
@@ -382,7 +382,7 @@ struct openssl_thread_config
 };
 openssl_thread_config openssl_thread_config_manager;
 
-boost::mutex*         openssl_thread_config::openssl_mutexes = nullptr;
+std::mutex*         openssl_thread_config::openssl_mutexes = nullptr;
 
 unsigned long openssl_thread_config::get_thread_id()
 {
@@ -410,7 +410,7 @@ openssl_thread_config::openssl_thread_config()
   if (CRYPTO_get_id_callback() == NULL &&
       CRYPTO_get_locking_callback() == NULL)
   {
-    openssl_mutexes = new boost::mutex[CRYPTO_num_locks()];
+    openssl_mutexes = new std::mutex[CRYPTO_num_locks()];
     CRYPTO_set_id_callback(&get_thread_id);
     CRYPTO_set_locking_callback(&locking_callback);
   }
